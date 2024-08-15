b7fba43e507bf1937936c41f67a5067e794681b8
Mbelenga <mbelengaterence@gmail.com>
2024-08-16 01:01:53 +0300

    to be updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,4 +39,11 @@ class Auth:
         user = self._db.add_user(email, hashed_password)
         return user
 
-
+    def valid_login(self, email: str, password: str) -> bool:
+        """ credentials validation """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                password_bytes = password.encode('utf-8')
+                hashed_password = user.hashed_password
+                if bcrypt.checkpw(password_bytes, hashed_password):
